50b8f5ead80200af37eb5fcf1cca196530b17b98
ZagustiTEL <troninila789@gmail.com>
2025-01-22 21:53:46 +0400

    Update import telebot.py

--- import telebot.py
+++ import telebot.py
@@ -1,9 +1,13 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('7737738429:AAE4itTACHj3DT3XBTQX9UFY-Txts6LKYWg')
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, text="Привет исследователь")
+    markup = types.InlineKeyboardMarkup()
+    file = open('Terraria_Calamity_Mod.jpg', 'rb')
+    caption = 'Привет исследователь'
+    bot.send_photo(message.chat.id,file,caption)
 
 bot.polling(none_stop=True) 
