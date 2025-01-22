571c82e5531bdc51489916143cbc2e3c9167918c
ZagustiTEL <troninila789@gmail.com>
2025-01-22 18:30:43 +0400

    Add files via upload

---
+++ import telebot.py
@@ -0,0 +1,10 @@
+import telebot
+
+
+bot = telebot.TeleBot('7737738429:AAE4itTACHj3DT3XBTQX9UFY-Txts6LKYWg')
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Привет исследователь")
+
+bot.polling(none_stop=True)
