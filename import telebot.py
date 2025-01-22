93b09b026a5a324285c3b8d9d50c9aa712fe6087
ZagustiTEL <troninila789@gmail.com>
2025-01-22 19:12:00 +0400

    Update import telebot.py

--- import telebot.py
+++ import telebot.py
@@ -4,6 +4,6 @@ bot = telebot.TeleBot('7737738429:AAE4itTACHj3DT3XBTQX9UFY-Txts6LKYWg')
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Привет исследователь")
+    bot.send_message(message.chat.id, text="Привет исследователь")
 
 bot.polling(none_stop=True) 
