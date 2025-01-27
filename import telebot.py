60324e69f98dd6592e44108e615cdd8bc021c36c
ZagustiTEL <troninila789@gmail.com>
2025-01-27 17:54:20 +0400

    Update import telebot.py

--- import telebot.py
+++ import telebot.py
@@ -1,25 +1,70 @@
-import telebot 
+import os
+import telebot
 from telebot import types
 
-bot = telebot.TeleBot('7737738429:AAE4itTACHj3DT3XBTQX9UFY-Txts6LKYWg')
+TOKEN = '7737738429:AAE4itTACHj3DT3XBTQX9UFY-Txts6LKYWg'
+
+bot = telebot.TeleBot(TOKEN)
+
+# Главное меню
+main_menu = types.InlineKeyboardMarkup(row_width=2)
+button_adon = types.InlineKeyboardButton(text='Адоны для Calamity', callback_data='adon')
+button_klass = types.InlineKeyboardButton(text='Классы', callback_data='klass')
+main_menu.add(button_adon, button_klass)
+
+# Меню адонов
+adon_menu = types.InlineKeyboardMarkup(row_width=2)
+button_catalist = types.InlineKeyboardButton(text='Calamity Catalist', url='https://terrariamods.wiki.gg/wiki/Catalyst')
+button_overhaul = types.InlineKeyboardButton(text='Calamity Overhaul', url='https://terrariamods.wiki.gg/wiki/Calamity_Overhaul')
+button_wotg = types.InlineKeyboardButton(text='Wrath of the Gods', url='https://terrariamods.wiki.gg/wiki/Wrath_of_the_Gods')
+button_back = types.InlineKeyboardButton(text='Назад', callback_data='back')
+adon_menu.add(button_catalist, button_overhaul, button_wotg, button_back)
+
+# Меню классов
+klass_menu = types.InlineKeyboardMarkup(row_width=2)
+button_shooter = types.InlineKeyboardButton(text='Стрелок🏹', callback_data='shooter')
+button_warrior = types.InlineKeyboardButton(text='Воин⚔️', callback_data='warrior')
+button_mag = types.InlineKeyboardButton(text='Маг🧙', callback_data='mag')
+button_summoner = types.InlineKeyboardButton(text='Призыватель🐴', callback_data='summoner')
+button_rogue = types.InlineKeyboardButton(text='Разбойник🥷', callback_data='rogue')
+klass_menu.add(button_shooter, button_warrior, button_mag, button_summoner, button_rogue, button_back)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    markup = types.InlineKeyboardMarkup()
-    bat1=types.InlineKeyboardButton('Адоны для Calamity', callback_data='adon')
-    bat2=types.InlineKeyboardButton('Классы', callback_data='klass')
-    markup.row(bat1,bat2)
-    file = open('Terraria_Calamity_Mod.jpg', 'rb')
-    caption = 'Привет исследователь'
-    bot.send_photo(message.chat.id,file,caption , reply_markup=markup )
-   
-@bot.callback_query_handler(func=lambda call: True)
-def answer(call):
-    if call.data == 'adon':
-        markup1 = types.InlineKeyboardMarkup()
-        bat3 = types.InlineKeyboardButton('Calamity Overhul', url='https://terrariamods.wiki.gg/wiki/Calamity_Overhaul')
-        bat4 = types.InlineKeyboardButton('Calamity Catalyst', url='https://terrariamods.wiki.gg/wiki/Catalyst')
-        markup1.add(bat3, bat4)
-        bot.edit_message_text(call.message.chat.id, "Выберите аддон:", reply_markup=markup1)
+    image_path = 'Terraria_Calamity_Mod.jpg'
+    if not os.path.exists(image_path):
+        bot.reply_to(message, "Извините, изображение недоступно.")
+        return
 
-bot.polling(none_stop=True)
+    try:
+        with open(image_path, 'rb') as photo:
+            bot.send_photo(
+                message.chat.id,
+                photo,
+                caption='Привет исследователь. Выберите категорию:',
+                reply_markup=main_menu
+            )
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка при отправке изображения: {e}")
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data == 'adon':
+        bot.edit_message_caption(chat_id=call.message.chat.id,
+                                 message_id=call.message.message_id,
+                                 caption='Вы выбрали адоны для Calamity',
+                                 reply_markup=adon_menu)
+        
+    elif call.data == 'klass':
+        bot.edit_message_caption(chat_id=call.message.chat.id,
+                                 message_id=call.message.message_id,
+                                 caption='Вы выбрали классы в Calamity',
+                                 reply_markup=klass_menu)
+    
+    elif call.data == 'back':
+        bot.edit_message_reply_markup(chat_id=call.message.chat.id,
+                                      message_id=call.message.message_id,
+                                      reply_markup=main_menu)
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
